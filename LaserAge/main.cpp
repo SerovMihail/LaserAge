6d76439cf069f7c67fbde581ba3453100bafd92d
Sasha Karyapin <Alitar1996@yandex.ru>
2016-12-25 21:41:37 +0300

    music

--- LaserAge/main.cpp
+++ LaserAge/main.cpp
@@ -1,9 +1,12 @@
 #include "GameManager.h"
+#include <SFML/Audio.hpp>
 
 int main()
 {
 	GameManager laserAge;
-	
+	sf::Music music;
+	music.openFromFile("images/8bit.ogg");
+	music.play();
 	int startLvl = 1;
 	laserAge.gameRunning(startLvl);	
 
